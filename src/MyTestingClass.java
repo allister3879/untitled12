32786f0c9b2135e819cde7bac4e1cc7f6866baed
allister3879 <220956@astanait.edu.kz>
2023-05-14 18:16:28 +0600

    getters and setters for class

--- src/MyTestingClass.java
+++ src/MyTestingClass.java
@@ -1,4 +1,19 @@
 public class MyTestingClass {
     private int key;
     private String value;
+    private MyTestingClass(int key){
+        this.key = key;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
