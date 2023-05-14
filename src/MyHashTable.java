a1148d92de6ce404781331a74f2f0b9d7f9860b8
allister3879 <220956@astanait.edu.kz>
2023-05-14 17:55:58 +0600

    creating variables for hash table

---
+++ src/MyHashTable.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class MyHashTable {
+    private ArrayList<LinkedList<HashNode<K, V>>> bucketArray;
+    private int numBuckets;
+    private int size;
+}
