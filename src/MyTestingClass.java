40b5bc388736010c8f8b8c0da2c26bb0b57877fc
allister3879 <220956@astanait.edu.kz>
2023-05-14 18:29:35 +0600

    implementing 'equals' method

--- src/MyTestingClass.java
+++ src/MyTestingClass.java
@@ -24,4 +24,23 @@ public class MyTestingClass {
         result = prime * result + ((value == null) ? 0 : value.hashCode()); // adding hash code of value
         return result;
     }
+
+    public boolean equals(Object obj) { // method if diff keys have same index
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        MyTestingClass other = (MyTestingClass) obj;
+        if (key != other.key)
+            return false;
+        if (value == null) {
+            if (other.value != null)
+                return false;
+        } else if (!value.equals(other.value))
+            return false;
+        return true;
+    }
+
 }
