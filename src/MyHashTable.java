e6bceb51371ab7705c2f24645f889b65d1a2393e
allister3879 <220956@astanait.edu.kz>
2023-05-14 17:57:20 +0600

    creating variables and constructor for inner class

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -5,4 +5,12 @@ public class MyHashTable {
     private ArrayList<LinkedList<HashNode<K, V>>> bucketArray;
     private int numBuckets;
     private int size;
+    private class HashNode<K, V> {
+        K key;
+        V value;
+        public HashNode(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
 }
